53fddb74aaacf88656855a3a7ec072436746e03d
aravindkarthik3096 <aravindkarthik3010@gmail.com>
2020-03-10 12:41:52 +0530

    Completed today task

--- src/test/java/Demo1.java
+++ src/test/java/Demo1.java
@@ -1,7 +1,11 @@
+import java.util.Date;
 
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("hi");
+		
+		Date d = new Date();
+		System.out.println("Done my work" + d);
 	}
 
 }
